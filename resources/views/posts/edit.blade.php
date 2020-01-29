5b689feb0b22dd3fc8aa7b6810b5a7c178281832
sergx <serg_x@bk.ru>
2020-01-29 16:06:56 +0300

    Laravel From Scratch [Part 8] - Edit & Delete Data

---
+++ resources/views/posts/edit.blade.php
@@ -0,0 +1,20 @@
+@extends('layouts.app')
+@section('content')
+    <h1>Edit post</h1>
+    {!! Form::open(['action' => ['PostsController@update', $post->id], 'method' => 'POST']) !!}
+    {{Form::hidden('_method', 'PUT')}}
+    <div class="form-group">
+      {{Form::label('title','Title')}}
+      {{Form::text('title', $post->title, ['class' => 'form-control','placeholder' => 'title'])}}
+    </div>
+    <div class="form-group">
+      {{Form::label('body','Body')}}
+      {{Form::textarea('body', $post->body, ['class' => 'form-control','placeholder' => 'Body text', 'id' => 'article-ckeditor'])}}
+    </div>
+    {{Form::submit('Submit', ['class' => 'btn btn-primary'])}}
+    {!! Form::close() !!}
+    <script src="/vendor/unisharp/laravel-ckeditor/ckeditor.js"></script>
+    <script>
+        CKEDITOR.replace( 'article-ckeditor' );
+    </script>
+@endsection
--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -9,5 +9,12 @@
     </div>
     <hr>
     <p><small>{{$post->created_at}}</small></p>
+    <hr>
+    <a href="/posts/{{$post->id}}/edit" class="btn btn-default">Edit</a>
+
+    {!!Form::open(['action' => ['PostsController@destroy', $post->id], 'method' => 'POST', 'class' => 'pull-right'])!!}
+      {{Form::hidden('_method','DELETE')}}
+      {{Form::submit('Delete',['class' => 'btn btn-danger'])}}
+    {!!Form::close()!!}
   </div>
 @endsection
